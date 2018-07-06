5b08559aed7b2fcad8be2449b11870c78b608dd4
stefanooldeman <stefano.oldeman@gmail.com>
2018-07-06 14:36:38 +0200

    Add generated boilerplate

---
+++ pkg/apis/sense12/v1/types.go
@@ -0,0 +1,36 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type AppServiceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []AppService `json:"items"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type AppService struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata"`
+	Spec              AppServiceSpec   `json:"spec"`
+	Status            AppServiceStatus `json:"status,omitempty"`
+}
+
+type AppServiceSpec struct {
+	// Name of the application
+	Name string `json:"name"`
+	// Docker image name including tag
+	Image string `json:"image"`
+}
+
+type AppServiceStatus struct {
+	// Latest stable image
+	StableImage string `json:"stableImage"`
+	// When upgrade is in progress a new image will be launced in Beta mode
+	UpgradeInProgress bool `json:"upgradeInProgress"`
+}
